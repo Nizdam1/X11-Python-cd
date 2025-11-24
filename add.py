b87420f978b24bc5c1bd67c6e1c6f4d0b0cfcf52
Nizdam <jpm1g25@soton.ac.uk>
2025-11-24 09:48:21 +0000

    fix the add function

--- add.py
+++ add.py
@@ -1,12 +1,5 @@
 def add(x: int, y: int) -> int:
-<<<<<<< HEAD
-   if not isinstance(x,int) or not isinstance(y,int):
-       raise TypeError("Both arguments must be integers")
-"""Returns the sum of two integers."""
-   return x + y
-=======
     if not isinstance(x,int) or not isinstance(y,int):
-        raise TypeError("Both arguments must be integers")
+       raise TypeError("Both arguments must be integers")
     """Returns the sum of two integers."""
     return x + y
->>>>>>> df5c0fe4220a4e66f50f34613eb0227e496a640c
